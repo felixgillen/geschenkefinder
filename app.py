f6fb6adc6eec29641daeb339685d3e08f61eca32
Felix Gillen <felix.gillen@coupies.de>
2016-10-15 13:25:56 +0200

    Ärger mit verschiedenen Anführungsstrichen

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def makeWebhookResult(req):
 
     speech = "Der Test hat funktioniert."
     
-    slack_message = "{
+    slack_message = '{
     "text": "Hallo!",
     "attachments": [
         {
@@ -72,7 +72,7 @@ def makeWebhookResult(req):
             ]
         }
     ]
-}"
+}'
 
 
     print("Response:")
